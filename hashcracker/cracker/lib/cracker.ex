ab3fae820c45897b23559ae282d0e66a8df1e5cf
justin <justinledford@gmail.com>
2017-11-05 13:42:49 -0600

    add basic brute force cracking and string generation

--- hashcracker/cracker/lib/cracker.ex
+++ hashcracker/cracker/lib/cracker.ex
@@ -1,18 +1,36 @@
 defmodule Cracker do
-  @moduledoc """
-  Documentation for Cracker.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cracker.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def crack(hash, hash_type, :brute) do
+    string_list(?\ ..?~)
+    |> generate(50)
+    |> Stream.map(fn x -> { x, :crypto.hash(hash_type, x) } end)
+    |> Enum.find(fn { _, hash_ } -> hash_ == hash end)
   end
+
+  ####
+  #
+  # Utilities for generating strings
+  #
+  ####
+
+  def string_list(range) do
+    for n <- range, do: << n :: utf8 >>
+  end
+
+  def _generate(source, target) do
+    Stream.flat_map(source, fn x ->
+      Stream.map(target,
+                 &(Enum.join([x, &1])))
+    end)
+  end
+
+  def generate(source, n) do
+    Enum.reduce(1..n, [source], fn _, [last | acc] ->
+      generated = _generate(source, last)
+      [generated | [last | acc]]
+    end)
+    |> Enum.reverse
+    |> Stream.concat
+  end
+
+
 end
--- hashcracker/cracker/test/cracker_test.exs
+++ hashcracker/cracker/test/cracker_test.exs
@@ -2,7 +2,12 @@ defmodule CrackerTest do
   use ExUnit.Case
   doctest Cracker
 
-  test "greets the world" do
-    assert Cracker.hello() == :world
+  test "test brute force cracking" do
+    hash_type = :md5
+    pass = "foo"
+    hash = :crypto.hash(hash_type, pass)
+    {pass_guess, hash_guess} = Cracker.crack(hash, hash_type, :brute)
+    assert pass == pass_guess
+    assert hash == hash_guess
   end
 end
