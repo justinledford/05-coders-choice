9855023dcdadc940be57a6586051e25811bf7fff
justin <justinledford@gmail.com>
2017-11-09 17:20:03 -0600

    remove generator from application start

--- hashcracker/cracker/lib/cracker/application.ex
+++ hashcracker/cracker/lib/cracker/application.ex
@@ -6,9 +6,6 @@ defmodule Cracker.Application do
       %{id: Cracker.Generator,
         start: {Cracker.Generator, :start_link, []},
         restart: :transient},
-      %{id: Cracker.Queue,
-        start: {Cracker.Queue, :start_link, []},
-        restart: :transient},
       %{id: Cracker.Dispatcher,
         start: {Cracker.Dispatcher, :start_link, []},
         restart: :transient}
