d6f380bceb516d2fbb6231a6c8ec9035c1d1e6f4
Justin Ledford <justinledford@gmail.com>
2017-11-10 11:28:36 -0600

    update tests

--- hashcracker/cracker/test/cracker_test.exs
+++ hashcracker/cracker/test/cracker_test.exs
@@ -6,19 +6,26 @@ defmodule CrackerTest do
     hash_type = :md5
     pass = "foo"
     hash = :crypto.hash(hash_type, pass)
-    {pass_guess, hash_guess} = Cracker.crack(hash, hash_type, :brute)
+    pass_guess = Cracker.crack(hash, hash_type, :brute, 1)
     assert pass == pass_guess
-    assert hash == hash_guess
   end
 
-  test "test wordlist cracking" do
+  test "test brute force cracking, multiple workers" do
     hash_type = :md5
-    pass = "password"
+    pass = "foo"
     hash = :crypto.hash(hash_type, pass)
-    {pass_guess, hash_guess} = Cracker.crack(hash, hash_type,
-                                             :dictionary, "wordlist.txt")
+    pass_guess = Cracker.crack(hash, hash_type, :brute, 4)
     assert pass == pass_guess
-    assert hash == hash_guess
   end
+
+  #test "test wordlist cracking" do
+  #  hash_type = :md5
+  #  pass = "password"
+  #  hash = :crypto.hash(hash_type, pass)
+  #  {pass_guess, hash_guess} = Cracker.crack(hash, hash_type,
+  #                                           :dictionary, "wordlist.txt")
+  #  assert pass == pass_guess
+  #  assert hash == hash_guess
+  #end
 
 end
