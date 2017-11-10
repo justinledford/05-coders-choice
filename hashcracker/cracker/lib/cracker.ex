faed659a0db69014eed64250c1a4e85c21f1c930
Justin Ledford <justinledford@gmail.com>
2017-11-10 10:52:58 -0600

    send found pass to client

--- hashcracker/cracker/lib/cracker.ex
+++ hashcracker/cracker/lib/cracker.ex
@@ -1,10 +1,23 @@
 defmodule Cracker do
-  #def crack(hash, hash_type, :brute) do
-  #  Cracker.Generator.init_ascii(50)
-  #  Cracker.Generator.get_stream()
-  #  |> find_matching_hash(hash, hash_type)
-  #end
+  def crack(hash, hash_type, :brute, num_workers) do
+    Cracker.Dispatcher.start_brute_force(num_workers, hash, :sha, self())
+    receive do
+      {:pass_found, pass} ->
+        pass
+      {:pass_not_found, nil} ->
+        nil
+    end
+  end
 
+  def crack(hash, hash_type, :mask, mask, num_workers) do
+    Cracker.Dispatcher.start_mask(num_workers, hash, :sha, mask, self())
+    receive do
+      {:pass_found, pass} ->
+        pass
+      {:pass_not_found, nil} ->
+        nil
+    end
+  end
   #def crack(hash, hash_type, :dictionary, wordlist_path) do
   #  Cracker.Generator.init_dictionary(wordlist_path)
   #  Cracker.Generator.get_stream()
--- hashcracker/cracker/lib/cracker/dispatcher.ex
+++ hashcracker/cracker/lib/cracker/dispatcher.ex
@@ -10,21 +10,23 @@ defmodule Cracker.Dispatcher do
   end
 
   # Just mask increment with mask ?a up to 64 (arbitrary upper bound)
-  def start_brute_force(num_workers, hash, hash_type) do
+  def start_brute_force(num_workers, hash, hash_type, client_pid) do
     mask = String.duplicate("?a", @brute_force_upper_bound)
     GenServer.cast(__MODULE__,
                    {:mask, num_workers, hash, hash_type,
-                     mask, 1, @brute_force_upper_bound})
+                     mask, 1, @brute_force_upper_bound, client_pid})
   end
 
-  def start_mask(num_workers, hash, hash_type, mask) do
+  def start_mask(num_workers, hash, hash_type, mask, client_pid) do
     GenServer.cast(__MODULE__,
-                   {:mask, num_workers, hash, hash_type, mask})
+                   {:mask, num_workers, hash, hash_type, mask, client_pid})
   end
 
-  def start_mask_increment(num_workers, hash, hash_type, mask, start, stop) do
+  def start_mask_increment(num_workers, hash, hash_type, mask,
+                           start, stop, client_pid) do
     GenServer.cast(__MODULE__,
-                   {:mask, num_workers, hash, hash_type, mask, start, stop})
+                   {:mask, num_workers, hash, hash_type,
+                     mask, start, stop, client_pid})
   end
 
   def not_found(pid) do
@@ -37,7 +39,7 @@ defmodule Cracker.Dispatcher do
 
   #####
   # GenServer implementation
-  def handle_cast({:mask, num_workers, hash, hash_type, mask}, _) do
+  def handle_cast({:mask, num_workers, hash, hash_type, mask, client_pid}, _) do
     # Start up workers
     workers = Enum.reduce(1..num_workers, [], fn _, workers ->
       {:ok, pid} = Cracker.Worker.start_link
@@ -53,11 +55,12 @@ defmodule Cracker.Dispatcher do
     |> Enum.map(fn {chunk, worker} ->
          Cracker.Worker.mask_attack(chunk, mask, hash, hash_type, worker)
        end)
-    {:noreply, {workers, hash, hash_type}}
+    {:noreply, {workers, hash, hash_type, client_pid}}
   end
 
   # TODO: DRY
-  def handle_cast({:mask, num_workers, hash, hash_type, mask, start, stop}, _) do
+  def handle_cast({:mask, num_workers, hash, hash_type,
+                   mask, start, stop, client_pid}, _) do
     # Start up workers
     workers = Enum.reduce(1..num_workers, [], fn _, workers ->
       {:ok, pid} = Cracker.Worker.start_link
@@ -74,15 +77,15 @@ defmodule Cracker.Dispatcher do
        Cracker.Worker.mask_attack_increment(chunk, mask, start, stop,
                                             hash, hash_type, worker)
        end)
-    {:noreply, {workers, hash, hash_type}}
+    {:noreply, {workers, hash, hash_type, client_pid}}
   end
 
   # Found pass, but last worker sent not found
-  def handle_cast({:not_found, _}, {[], hash, hash_type}) do
+  def handle_cast({:not_found, _}, {[], hash, hash_type, client_pid}) do
     {:noreply, {[], hash, hash_type}}
   end
 
-  def handle_cast({:not_found, pid}, {[ _ | [] ], hash, hash_type}) do
+  def handle_cast({:not_found, pid}, {[ _ | [] ], hash, hash_type, client_pid}) do
     case Process.alive?(pid) do
       true ->
         GenServer.stop(pid)
@@ -94,7 +97,7 @@ defmodule Cracker.Dispatcher do
     {:noreply, {[], hash, hash_type}}
   end
 
-  def handle_cast({:not_found, pid}, {workers, hash, hash_type}) do
+  def handle_cast({:not_found, pid}, {workers, hash, hash_type, client_pid}) do
     case Process.alive?(pid) do
       true ->
         GenServer.stop(pid)
@@ -105,13 +108,12 @@ defmodule Cracker.Dispatcher do
     {:noreply, {workers, hash, hash_type}}
   end
 
-  def handle_cast({:found_pass, pass}, {workers, hash, hash_type}) do
+  def handle_cast({:found_pass, pass}, {workers, hash, hash_type, client_pid}) do
     Enum.map(workers, fn worker ->
       Process.exit(worker, :kill)
     end)
-    IO.puts pass
-    IO.puts DateTime.utc_now()
     # TODO: report to client that pass found
+    send client_pid, {:pass_found, pass}
     {:noreply, {workers, hash, hash_type}}
   end
 end
