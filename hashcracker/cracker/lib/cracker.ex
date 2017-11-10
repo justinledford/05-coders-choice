4d8bf203a231b2e7eb488e9b65c53704b714d499
Justin Ledford <justinledford@gmail.com>
2017-11-10 11:25:19 -0600

    add api funcs

--- hashcracker/cracker/lib/cracker.ex
+++ hashcracker/cracker/lib/cracker.ex
@@ -1,6 +1,6 @@
 defmodule Cracker do
   def crack(hash, hash_type, :brute, num_workers) do
-    Cracker.Dispatcher.start_brute_force(num_workers, hash, :sha, self())
+    Cracker.Dispatcher.start_brute_force(num_workers, hash, hash_type, self())
     receive do
       {:pass_found, pass} ->
         pass
@@ -10,7 +10,7 @@ defmodule Cracker do
   end
 
   def crack(hash, hash_type, :mask, mask, num_workers) do
-    Cracker.Dispatcher.start_mask(num_workers, hash, :sha, mask, self())
+    Cracker.Dispatcher.start_mask(num_workers, hash, hash_type, mask, self())
     receive do
       {:pass_found, pass} ->
         pass
@@ -18,9 +18,15 @@ defmodule Cracker do
         nil
     end
   end
-  #def crack(hash, hash_type, :dictionary, wordlist_path) do
-  #  Cracker.Generator.init_dictionary(wordlist_path)
-  #  Cracker.Generator.get_stream()
-  #  |> find_matching_hash(hash, hash_type)
-  #end
+
+  def crack(hash, hash_type, :mask_increment, mask, start, stop, num_workers) do
+    Cracker.Dispatcher.start_mask_increment(num_workers, hash, hash_type,
+                                            mask, start, stop, self())
+    receive do
+      {:pass_found, pass} ->
+        pass
+      {:pass_not_found, nil} ->
+        nil
+    end
+  end
 end
