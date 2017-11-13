f984e673a5c5e0ab9d2fb86594a767e65228ce0c
Justin Ledford <justinledford@gmail.com>
2017-11-13 12:30:46 -0600

    update tests

--- hashcracker/cracker/test/cracker_test.exs
+++ hashcracker/cracker/test/cracker_test.exs
@@ -2,7 +2,7 @@ defmodule CrackerTest do
   use ExUnit.Case
   doctest Cracker
 
-  test "test brute force cracking" do
+  test "brute force attack" do
     hash_type = :md5
     pass = "foo"
     hash = :crypto.hash(hash_type, pass)
@@ -10,7 +10,7 @@ defmodule CrackerTest do
     assert pass == pass_guess
   end
 
-  test "test brute force cracking, multiple workers" do
+  test "brute force attack, multiple workers" do
     hash_type = :md5
     pass = "foo"
     hash = :crypto.hash(hash_type, pass)
@@ -18,14 +18,43 @@ defmodule CrackerTest do
     assert pass == pass_guess
   end
 
-  #test "test wordlist cracking" do
+  test "mask attack" do
+    hash_type = :md5
+    pass = "foo"
+    hash = :crypto.hash(hash_type, pass)
+    pass_guess = Cracker.crack(hash, hash_type, :mask, "?l?l?l", 1)
+    assert pass == pass_guess
+  end
+
+  test "mask attack 1 char mask" do
+    hash_type = :md5
+    pass = "f"
+    hash = :crypto.hash(hash_type, pass)
+    pass_guess = Cracker.crack(hash, hash_type, :mask, "?l", 1)
+    assert pass == pass_guess
+  end
+
+  test "mask attack, multiple workers" do
+    hash_type = :md5
+    pass = "foo"
+    hash = :crypto.hash(hash_type, pass)
+    pass_guess = Cracker.crack(hash, hash_type, :mask, "?l?l?l", 4)
+    assert pass == pass_guess
+  end
+
+  #test "dictionary attack" do
   #  hash_type = :md5
-  #  pass = "password"
+  #  pass = "foo"
   #  hash = :crypto.hash(hash_type, pass)
-  #  {pass_guess, hash_guess} = Cracker.crack(hash, hash_type,
-  #                                           :dictionary, "wordlist.txt")
+  #  pass_guess = Cracker.crack(hash, hash_type, :dictionary, "wordlist.txt", 1)
   #  assert pass == pass_guess
-  #  assert hash == hash_guess
   #end
 
+  #test "dictionary attack, multiple workers" do
+  #  hash_type = :md5
+  #  pass = "foo"
+  #  hash = :crypto.hash(hash_type, pass)
+  #  pass_guess = Cracker.crack(hash, hash_type, :dictionary, "wordlist.txt", 4)
+  #  assert pass == pass_guess
+  #end
 end
