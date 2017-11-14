685d7231dff9452f91aa8186912b4d507d8df867
Justin Ledford <justinledford@gmail.com>
2017-11-13 20:17:26 -0600

    set up escript for client

--- hashcracker/client/mix.exs
+++ hashcracker/client/mix.exs
@@ -7,8 +7,13 @@ defmodule Client.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript
     ]
+  end
+
+  def escript do
+    [main_module: Client]
   end
 
   # Run "mix help compile.app" to learn about applications.
