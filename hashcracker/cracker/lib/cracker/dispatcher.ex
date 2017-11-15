a4eb4b4147556f0ee2f808328b429c763e657f5a
justin <justinledford@gmail.com>
2017-11-14 20:24:23 -0600

    refactor worker/dispatcher

--- hashcracker/cracker/lib/cracker/dispatcher.ex
+++ hashcracker/cracker/lib/cracker/dispatcher.ex
@@ -3,8 +3,10 @@ defmodule Cracker.Dispatcher do
 
   @brute_force_upper_bound 64
 
-  #####
+  ##################################################
   # External API
+  ##################################################
+
   def start_link do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
@@ -26,8 +28,7 @@ defmodule Cracker.Dispatcher do
   ##################################################
 
   def handle_cast({:not_found, worker}, state) do
-    alive = Process.alive?(worker)
-    state = worker_done(worker, state, Enum.count(state.workers), alive)
+    state = worker_done(worker, state, Enum.count(state.workers))
     {:noreply, state}
   end
 
@@ -44,17 +45,17 @@ defmodule Cracker.Dispatcher do
     {:noreply, state}
   end
 
+  ##################################################
+  # Implementation
+  ##################################################
 
-  #####
-  #
-
-  def start_workers(num_workers) do
+  defp start_workers(num_workers) do
     {:ok, _} = Cracker.DispatcherSupervisor.start_link(num_workers)
     Supervisor.which_children(Cracker.DispatcherSupervisor)
     |> Enum.map(fn {_, pid, _, _} -> pid end)
   end
 
-  def split_mask_head_tail(mask) do
+  defp split_mask_head_tail(mask) do
     case String.split(mask, "?", trim: true, parts: 2) do
       [ h | [ mask ] ] ->
         {h, mask}
@@ -63,15 +64,15 @@ defmodule Cracker.Dispatcher do
     end
   end
 
-  def dispatch(state=%{attack: :brute}) do
+  defp dispatch(state=%{attack: :brute}) do
     mask = String.duplicate("?a", @brute_force_upper_bound)
     state
     |> Map.merge(%{mask: mask, start: 1, stop: @brute_force_upper_bound})
-    |> Map.put(:attack, :mask_increment)
+    |> Map.put(:attack, :mask)
     |> dispatch
   end
 
-  def dispatch(state=%{attack: :mask}) do
+  defp dispatch(state=%{attack: :mask}) do
     {mask_h, mask_t} = split_mask_head_tail(state.mask)
     [ h_enum | _ ] = Cracker.Util.mask_to_enums(mask_h)
     h_enum
@@ -81,54 +82,35 @@ defmodule Cracker.Dispatcher do
       fn {chunk, worker} ->
         Map.drop(state, [:workers])
         |> Map.merge(%{chunk: chunk, worker: worker, mask_t: mask_t})
-        |> Cracker.Worker.mask_attack
+        |> Cracker.Worker.start_work
       end)
   end
 
-  def dispatch(state=%{attack: :mask_increment}) do
-    {mask_h, mask_t} = split_mask_head_tail(state.mask)
-    [ h_enum | _ ] = Cracker.Util.mask_to_enums(mask_h)
-    h_enum
-    |> Cracker.Util.chunk(state.num_workers)
-    |> Enum.zip(state.workers)
-    |> Enum.map(
-      fn {chunk, worker} ->
-        Map.drop(state, [:workers])
-        |> Map.merge(%{chunk: chunk, worker: worker, mask_t: mask_t})
-        |> Cracker.Worker.mask_attack_increment
-      end)
-  end
-
-  def dispatch(state=%{attack: :dictionary}) do
+  defp dispatch(state=%{attack: :dictionary}) do
     chunk_size = state.wordlist_path
     |> File.stat!
     |> Map.get(:size)
     |> div(state.num_workers)
 
-    chunk_bounds = Enum.map(0..state.num_workers-1, fn i ->
-      {i*chunk_size, (i+1)*chunk_size}
+    starts = Enum.map(0..state.num_workers-1, fn i ->
+      i*chunk_size
     end)
 
-    Enum.zip(state.workers, chunk_bounds)
-    |> Enum.map(fn {worker, {start, stop}} ->
+    Enum.zip(state.workers, starts)
+    |> Enum.map(fn {worker, start} ->
         state
         |> Map.drop([:workers])
-        |> Map.merge(%{worker: worker, start: start, stop: stop})
-        |> Cracker.Worker.dictionary_attack
+        |> Map.merge(%{worker: worker, start: start, chunk_size: chunk_size})
+        |> Cracker.Worker.start_work
     end)
   end
 
-  def worker_done(_worker, state, 1, _alive) do
+  defp worker_done(_worker, state, worker_count) when worker_count < 2 do
     Supervisor.stop(Cracker.DispatcherSupervisor)
     send state.client_pid, {:pass_not_found, nil}
     state
   end
-  def worker_done(worker, state, _worker_count, true) do
-    Supervisor.terminate_child(Cracker.DispatcherSupervisor, worker)
-    workers = Enum.filter(state.workers, fn worker_ -> worker_ != worker end)
-    Map.put(state, :workers, workers)
-  end
-  def worker_done(worker, state, _worker_count, false) do
+  defp worker_done(worker, state, _) do
     workers = Enum.filter(state.workers, fn worker_ -> worker_ != worker end)
     Map.put(state, :workers, workers)
   end
--- hashcracker/cracker/lib/cracker/worker.ex
+++ hashcracker/cracker/lib/cracker/worker.ex
@@ -1,79 +1,55 @@
 defmodule Cracker.Worker do
   use GenServer
 
-  #####
+  ##################################################
   # External API
+  ##################################################
+
   def start_link(num) do
     GenServer.start_link(__MODULE__, [], name: String.to_atom("Worker-#{num}"))
   end
 
-  # Take enum and start hasing
-  def start_work(enum, hash, hash_type, pid) do
-    GenServer.cast(pid, {:work, enum, hash, hash_type})
+  def start_work(state) do
+    GenServer.cast(state.worker, {:start_work, state})
   end
 
-  # Given enums, get product to generate candidates
-  def mask_attack(state) do
-    GenServer.cast(state.worker, {:mask_attack, state})
-  end
-
-  def mask_attack_increment(state) do
-    GenServer.cast(state.worker, {:mask_attack_increment, state})
-  end
-
-  def dictionary_attack(state) do
-    GenServer.cast(state.worker, {:dictionary_attack, state})
-  end
-
-  #####
+  ##################################################
   # GenServer implementation
-  def handle_cast({:work, enum, hash, hash_type}, _) do
-    enum
-    |> Cracker.Cracker.find_matching_hash(hash, hash_type)
-    |> message_dispatcher
+  ##################################################
+
+  def handle_cast({:start_work, state}, _) do
+    attack(state)
     {:noreply, nil}
   end
 
-  def handle_cast({:mask_attack, state}, _) do
+  ##################################################
+  # Implementation
+  ##################################################
+
+  defp attack(state=%{attack: :mask, start: start, stop: stop}) do
+    tail_enums = Cracker.Util.mask_to_enums(state.mask_t)
+    enums = [ state.chunk | tail_enums ]
+    enums_partial = Enum.take(enums, start)
+    enums = Enum.drop(enums, start)
+    Map.merge(state, %{enums: enums, enums_partial: enums_partial})
+    |> mask_increment_loop(stop-start, [[]])
+    |> message_dispatcher
+  end
+
+  defp attack(state=%{attack: :mask}) do
     tail_enums = Cracker.Util.mask_to_enums(state.mask_t)
     enums = [ state.chunk | tail_enums ]
     Cracker.Util.product(enums)
     |> Stream.map(&Enum.join/1)
     |> Cracker.Cracker.find_matching_hash(state.hash, state.hash_type)
     |> message_dispatcher
-    {:noreply, nil}
   end
 
-  def handle_cast({:mask_attack_increment, state}, _) do
-    tail_enums = Cracker.Util.mask_to_enums(state.mask_t)
-    enums = [ state.chunk | tail_enums ]
-
-    enums_partial = Enum.take(enums, state.start)
-    enums = Enum.drop(enums, state.start)
-    Map.merge(state, %{enums: enums, enums_partial: enums_partial})
-    |> mask_increment_loop(state.stop-state.start, [[]])
-    |> message_dispatcher
-
-    {:noreply, nil}
-  end
-
-  def handle_cast({:dictionary_attack, state}, _) do
-    f = File.open!(state.wordlist_path, [:read_ahead])
-    {:ok, _} = :file.position(f, state.start)
-
-    case state.start do
-      0 ->
-        nil
-      _ ->
-      # seek file to next non newline
-        IO.read(f, :line)
-    end
-
-    f
-    |> IO.stream(:line)
+  defp attack(state=%{attack: :dictionary}) do
+    wordlist_stream(state.wordlist_path, state.start)
     |> Stream.map(&String.trim_trailing/1)
     |> Stream.transform(0, fn line, bytes_read ->
-         if (state.stop-state.start) > bytes_read do
+         if bytes_read < state.chunk_size do
            {[line], bytes_read + (String.length line) + 1}
          else
            {:halt, bytes_read}
@@ -81,33 +57,28 @@ defmodule Cracker.Worker do
        end)
     |> Cracker.Cracker.find_matching_hash(state.hash, state.hash_type)
     |> message_dispatcher
-
-    {:noreply, nil}
   end
 
-  #####
-  #
-
-  def mask_increment_loop(_state,-1,_results) do
+  defp mask_increment_loop(_state,-1,_results) do
     nil
   end
-  def mask_increment_loop(state, i, results) do
+  defp mask_increment_loop(state, i, results) do
     results = Cracker.Util.product(state.enums_partial, results)
-    found = results
+    results
     |> Stream.map(&Enum.join/1)
     |> Cracker.Cracker.find_matching_hash(state.hash, state.hash_type)
+    |> _mask_increment_loop(state, i, results)
+  end
 
-    case found do
-      nil ->
-        enums_partial = Enum.take(state.enums, 1)
-        enums = Enum.drop(state.enums, 1)
-
-        state
-        |> Map.merge(%{enums: enums, enums_partial: enums_partial})
-        |> mask_increment_loop(i-1, results)
-      found ->
-        found
-    end
+  defp _mask_increment_loop(nil, state, i, results) do
+    enums_partial = Enum.take(state.enums, 1)
+    enums = Enum.drop(state.enums, 1)
+    state
+    |> Map.merge(%{enums: enums, enums_partial: enums_partial})
+    |> mask_increment_loop(i-1, results)
+  end
+  defp _mask_increment_loop(found, _, _, _) do
+    found
   end
 
   defp message_dispatcher(nil) do
@@ -116,6 +87,20 @@ defmodule Cracker.Worker do
 
   defp message_dispatcher({pass, _}) do
     Cracker.Dispatcher.found_pass(pass)
+  end
+
+  defp wordlist_stream(wordlist_path, start) do
+    f = File.open!(wordlist_path, [:read_ahead])
+    {:ok, _} = :file.position(f, start)
+    seek_file(f, start)
+    IO.stream(f, :line)
+  end
+
+  defp seek_file(_, 0) do
+    nil
+  end
+  defp seek_file(f, _) do
+    IO.read(f, :line)
   end
 
 end
